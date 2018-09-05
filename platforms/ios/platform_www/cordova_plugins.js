b3462366af61ec45754b37dab33cd81bc807e2a9
Adam George <github@adamgeorge.com>
2018-09-05 15:47:14 +1000

    Adding phonegap-plugin-push

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -9,13 +9,22 @@ module.exports = [
       "cordova.plugins.intercom",
       "plugin.intercom"
     ]
+  },
+  {
+    "id": "phonegap-plugin-push.PushNotification",
+    "file": "plugins/phonegap-plugin-push/www/push.js",
+    "pluginId": "phonegap-plugin-push",
+    "clobbers": [
+      "PushNotification"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-intercom": "6.1.0"
+  "cordova-plugin-intercom": "6.1.0",
+  "phonegap-plugin-push": "2.2.3"
 };
 // BOTTOM OF METADATA
 });
