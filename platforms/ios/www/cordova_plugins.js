631bb2c3a53e6f9f4856cd6afd957c0c6f92b766
Adam George <github@adamgeorge.com>
2018-09-05 15:44:22 +1000

    Adding cordova-plugin-intercom

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,21 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-intercom.Intercom",
+    "file": "plugins/cordova-plugin-intercom/www/intercom.js",
+    "pluginId": "cordova-plugin-intercom",
+    "clobbers": [
+      "intercom",
+      "cordova.plugins.intercom",
+      "plugin.intercom"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-intercom": "6.1.0"
 };
 // BOTTOM OF METADATA
 });
